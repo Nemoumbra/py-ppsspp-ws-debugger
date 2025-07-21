4f23b2d093e907baadddf1a857bdaacca52761dc
Nemoumbra <garbuzov.nickolay@gmail.com>
2025-07-21 22:54:30 +0300

    Implemented the HLE event parser + codestyle

--- src/ppsspp/parsers/detailed_parsers/gpu/gpu_event_parser.py
+++ src/ppsspp/parsers/detailed_parsers/gpu/gpu_event_parser.py
@@ -1,5 +1,6 @@
 
 from src.ppsspp.parsers.base_event_parser import BaseEventParser
+
 from src.ppsspp.model.events.gpu.common import (
     GpuRecordDumpEvent, GpuStatsGetEvent
 )
@@ -17,12 +18,13 @@ class GPUEventParser(BaseEventParser):
         lookup_table = {
             "gpu.record.dump": GpuRecordDumpEvent,
             "gpu.stats.get": GpuStatsGetEvent,
+
             "gpu.buffer.screenshot": GpuBufferScreenshotUriEvent | GpuBufferScreenshotB64Event,
             "gpu.buffer.renderColor": GpuBufferRenderColorUriEvent | GpuBufferRenderColorB64Event,
             "gpu.buffer.renderDepth": GpuBufferRenderDepthUriEvent | GpuBufferRenderDepthB64Event,
             "gpu.buffer.renderStencil": GpuBufferRenderStencilUriEvent | GpuBufferRenderStencilB64Event,
             "gpu.buffer.texture": GpuBufferTextureUriEvent | GpuBufferTextureB64Event,
-            "gpu.buffer.clut": GpuBufferClutUriEvent | GpuBufferClutB64Event
+            "gpu.buffer.clut": GpuBufferClutUriEvent | GpuBufferClutB64Event,
         }
 
         BaseEventParser.__init__(self, lookup_table)
--- src/ppsspp/parsers/detailed_parsers/hle/hle_event_parser.py
+++ src/ppsspp/parsers/detailed_parsers/hle/hle_event_parser.py
@@ -1,3 +1,34 @@
 
-class HLEEventParser:
-    pass
+from src.ppsspp.parsers.base_event_parser import BaseEventParser
+
+from src.ppsspp.model.events.hle.thread import (
+    HleThreadListEvent, HleThreadStopEvent, HleThreadWakeEvent
+)
+from src.ppsspp.model.events.hle.func import (
+    HleFuncListEvent, HleFuncAddEvent, HleFuncRemoveEvent,
+    HleFuncRemoveRangeEvent, HleFuncRenameEvent, HleFuncScanEvent
+)
+from src.ppsspp.model.events.hle.common import (
+    HleModuleListEvent, HleBacktraceEvent
+)
+
+
+class HLEEventParser(BaseEventParser):
+    def __init__(self):
+        lookup_table = {
+            "hle.thread.list": HleThreadListEvent,
+            "hle.thread.wake": HleThreadWakeEvent,
+            "hle.thread.stop": HleThreadStopEvent,
+
+            "hle.func.list": HleFuncListEvent,
+            "hle.func.add": HleFuncAddEvent,
+            "hle.func.remove": HleFuncRemoveEvent,
+            "hle.func.removeRange": HleFuncRemoveRangeEvent,
+            "hle.func.rename": HleFuncRenameEvent,
+            "hle.func.scan": HleFuncScanEvent,
+
+            "hle.module.list": HleModuleListEvent,
+            "hle.backtrace": HleBacktraceEvent,
+        }
+
+        BaseEventParser.__init__(self, lookup_table)
