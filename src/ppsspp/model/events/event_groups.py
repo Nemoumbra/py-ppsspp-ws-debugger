4169094e1b7bca404377140187a259e099ecdf10
Nemoumbra <garbuzov.nickolay@gmail.com>
2025-07-21 20:25:54 +0300

    Starting moving to adaptix

--- src/ppsspp/model/events/event_groups.py
+++ src/ppsspp/model/events/event_groups.py
@@ -1,22 +1,22 @@
 
 from typing import Type
 
-from src.ppsspp.model.events.game.game_start import GameStartEvent
-from src.ppsspp.model.events.game.game_quit import GameQuitEvent
-from src.ppsspp.model.events.game.game_pause import GamePauseEvent
-from src.ppsspp.model.events.game.game_resume import GameResumeEvent
+from src.ppsspp.model.events.game.common import (
+    GameStartEvent, GameQuitEvent, GamePauseEvent, GameResumeEvent
+)
 
 from src.ppsspp.model.events.other.log import LogEvent
 
-from src.ppsspp.model.events.input.analog.input_analog import InputAnalogEvent
-from src.ppsspp.model.events.input.buttons.input_buttons import InputButtonsEvent
+from src.ppsspp.model.events.input.analog import InputAnalogEvent
+from src.ppsspp.model.events.input.buttons import InputButtonsEvent
 
-from src.ppsspp.model.events.cpu.cpu_stepping import SteppingEvent
-from src.ppsspp.model.events.cpu.cpu_resume import ResumedEvent
+from src.ppsspp.model.events.cpu.common import CpuSteppingEvent, CpuResumeEvent
+
+
 
 kLoggingEvents: set[Type] = {LogEvent}
 kGameEvents: set[Type] = {GameStartEvent, GamePauseEvent, GameQuitEvent, GameResumeEvent}
 kInputEvents: set[Type] = {InputAnalogEvent, InputButtonsEvent}
-kCpuEvents: set[Type] = {SteppingEvent, ResumedEvent}
+kCpuEvents: set[Type] = {CpuSteppingEvent, CpuResumeEvent}
 
 kBroadcastEvents = kLoggingEvents.union(kGameEvents).union(kInputEvents).union(kCpuEvents)
